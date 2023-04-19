42001ae75bd93841c63d16eef372f762ce03cea0
Manurbhav Arya <110805836+mannubhai1@users.noreply.github.com>
2023-04-20 00:38:10 +0530

    Add files via upload

---
+++ static/round1.js
@@ -0,0 +1,123 @@
+let check = 0;
+
+function q(id) {
+  a = document.getElementById('pop');
+  if (check == 0) {
+    a.style.display = "block";
+    if (id == 'qq1') {
+      document.getElementById('qest').innerHTML = "Name a number that is long lasting?";
+    }
+    else if (id == 'qq2') {
+      document.getElementById('qest').innerHTML = "After you take a selfie, who might you send it to?";
+    }
+    else if (id == 'qq3') {
+      document.getElementById('qest').innerHTML = "What is the first thing making noise in the morning, besides an alarm!";
+    }
+    else if (id == 'qq4') {
+      document.getElementById('qest').innerHTML = "Name something parents tell their kids not to waste!";
+    }
+    else if (id == 'qq5') {
+      document.getElementById('qest').innerHTML = "Besides a clock, watch or your telephone, what in your home displays the time?";
+    }
+    else if (id == 'qq6') {
+      document.getElementById('qest').innerHTML = "What might a boyfriend rather admit to his buddies than to his girlfiriend?";
+    }
+    else if (id == 'qq7') {
+      document.getElementById('qest').innerHTML = "Name something you would wear, even if it had a hole in it!";
+    }
+    else if (id == 'qq8') {
+      document.getElementById('qest').innerHTML = "Tell me something a man might do to get a woman 'in the mood'.";
+    }
+    else if (id == 'qq9') {
+      document.getElementById('qest').innerHTML = "What thing women exaggerates about herself?";
+    }
+    else if (id == 'qq10') {
+      document.getElementById('qest').innerHTML = "Besides a pillow, name something you might rest your head on to go sleep?";
+    }
+    else if (id == 'qq11') {
+      document.getElementById('qest').innerHTML = "Name Something You Love to Smell in the Morning.";
+    }
+    else if (id == 'qq12') {
+      document.getElementById('qest').innerHTML = "Name a movie you cant watch with your parent.";
+    }
+    else if (id == 'qq13') {
+      document.getElementById('qest').innerHTML = "Name a language that makes anything sound sexy.";
+    } else if (id == 'qq14') {
+      document.getElementById('qest').innerHTML = "What is the most cringe thing between couples?";
+    }
+    else if (id == 'qq15') {
+      document.getElementById('qest').innerHTML = "Name something that is the hardest to do, when it is your first time trying it and you are just learning.";
+    }
+    else if (id == 'qq16') {
+      document.getElementById('qest').innerHTML = "Name something your spouse might get mad at you for doing too much.";
+    }
+    else {
+      document.getElementById('qest').innerHTML = "?";
+    }
+    check = 1;
+  }
+  else {
+    a.style.display = "none";
+    check = 0;
+  }
+}
+let score1 = 0;
+let score2 = 0;
+let score3 = 0;
+let score4 = 0;
+let score5 = 0;
+
+function plusupdate(team) {
+  if (team == 't1') {
+    score1 = score1 + 100;
+    document.getElementById('score1').innerHTML = score1;
+  }
+  if (team == 't2') {
+    score2 = score2 + 100;
+    document.getElementById('score2').innerHTML = score2;
+  }
+  if (team == 't3') {
+    score3 = score3 + 100;
+    document.getElementById('score3').innerHTML = score3;
+  }
+  if (team == 't4') {
+    score4 = score4 + 100;
+    document.getElementById('score4').innerHTML = score4;
+  }
+  if (team == 't5') {
+    score5 = score5 + 100;
+    document.getElementById('score5').innerHTML = score5;
+  }
+  if (team == 't6') {
+    score6 = score6 + 100;
+    document.getElementById('score6').innerHTML = score6;
+  }
+
+}
+function minusupdate(team) {
+  if (team == 't1') {
+    score1 = score1 - 100;
+    document.getElementById('score1').innerHTML = score1;
+
+  }
+  if (team == 't2') {
+    score2 = score2 - 100;
+    document.getElementById('score2').innerHTML = score2;
+  }
+  if (team == 't3') {
+    score3 = score3 - 100;
+    document.getElementById('score3').innerHTML = score3;
+  }
+  if (team == 't4') {
+    score4 = score4 - 100;
+    document.getElementById('score4').innerHTML = score4;
+  }
+  if (team == 't5') {
+    score5 = score5 - 100;
+    document.getElementById('score5').innerHTML = score5;
+  }
+  if (team == 't6') {
+    score6 = score6 - 100;
+    document.getElementById('score6').innerHTML = score6;
+  }
+}
---
+++ static/round2.js
@@ -0,0 +1,129 @@
+const A = ['Lollipop (33)', 'Icecream (32)', 'Stamp (11)', 'Popsicle (10)', 'Envelope (8)', 'Lips (2)', 'Cash (1)', '']; // put your options here
+const B = ['Bread (36)', 'Pizza (28)', 'Cake (9)', 'Pie (6)', 'Cheese (5)', '', '', ''];
+const C = ['In a relationship (36)', 'Not interested (32)', 'Busy (17)', '', '', '', '', ''];
+
+let current = A;
+let qdis = "Question 1";
+
+let check1 = 0,
+  check2 = 0,
+  check3 = 0,
+  check4 = 0,
+  check5 = 0,
+  check6 = 0,
+  check7 = 0,
+  check8 = 0;
+
+function initialize() {
+  document.getElementById("qdisplay").innerHTML = qdis;
+  document.getElementById("text1").innerHTML = current[0];
+  document.getElementById("text2").innerHTML = current[1];
+  document.getElementById("text3").innerHTML = current[2];
+  document.getElementById("text4").innerHTML = current[3];
+  document.getElementById("text5").innerHTML = current[4];
+  document.getElementById("text6").innerHTML = current[5];
+  document.getElementById("text7").innerHTML = current[6];
+  document.getElementById("text8").innerHTML = current[7];
+}
+
+initialize();
+
+function change() {
+  var e = document.getElementById("ques");
+  var value = e.value;
+  qdis = value;
+
+  console.log(value);
+
+  if (value == "Question 1") {
+    current = A;
+    qdis = "Name Something That Has to Be Licked.";
+  }
+
+  if (value == "Question 2") {
+    current = B;
+    qdis = "Name something you eat by the slice!";
+  }
+
+  if (value == "Question 3") {
+    current = C;
+    qdis = "If someone asked you out on a date, what's an excuse you might give if you weren't interested?";
+  }
+
+  for (var i = 0; i < 8; i++) {
+    id = "text" + (i + 1);
+    var a = document.getElementById(id);
+    a.style.display = "none";
+  }
+
+  check1 = 0,
+    check2 = 0,
+    check3 = 0,
+    check4 = 0,
+    check5 = 0,
+    check6 = 0,
+    check7 = 0,
+    check8 = 0;
+
+  initialize();
+}
+
+function display_opt(id) {
+  var a = document.getElementById(id);
+  if (id == "text1" && check1 == 0) {
+    a.style.display = "block";
+    check1 = 1;
+  } else if (id == "text1" && check1 == 1) {
+    a.style.display = "none";
+    check1 = 0;
+  }
+  if (id == "text2" && check2 == 0) {
+    a.style.display = "block";
+    check2 = 1;
+  } else if (id == "text2" && check2 == 1) {
+    a.style.display = "none";
+    check2 = 0;
+  }
+  if (id == "text3" && check3 == 0) {
+    a.style.display = "block";
+    check3 = 1;
+  } else if (id == "text3" && check3 == 1) {
+    a.style.display = "none";
+    check3 = 0;
+  }
+  if (id == "text4" && check4 == 0) {
+    a.style.display = "block";
+    check4 = 1;
+  } else if (id == "text4" && check4 == 1) {
+    a.style.display = "none";
+    check4 = 0;
+  }
+  if (id == "text5" && check5 == 0) {
+    a.style.display = "block";
+    check5 = 1;
+  } else if (id == "text5" && check5 == 1) {
+    a.style.display = "none";
+    check5 = 0;
+  }
+  if (id == "text6" && check6 == 0) {
+    a.style.display = "block";
+    check6 = 1;
+  } else if (id == "text6" && check6 == 1) {
+    a.style.display = "none";
+    check6 = 0;
+  }
+  if (id == "text7" && check7 == 0) {
+    a.style.display = "block";
+    check7 = 1;
+  } else if (id == "text7" && check7 == 1) {
+    a.style.display = "none";
+    check7 = 0;
+  }
+  if (id == "text8" && check8 == 0) {
+    a.style.display = "block";
+    check8 = 1;
+  } else if (id == "text8" && check8 == 1) {
+    a.style.display = "none";
+    check8 = 0;
+  }
+}
